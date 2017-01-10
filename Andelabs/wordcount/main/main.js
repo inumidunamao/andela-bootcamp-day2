9c120b5d742e0efc63d1762c4013fe97d639125d
Inumidun Amao <seindelawson@yahoo.com>
2017-01-10 15:45:09 +0100

    Updating wordcount code, bug fixes, and adding restrictions for edge cases

--- Andelabs/wordcount/main/main.js
+++ Andelabs/wordcount/main/main.js
@@ -1,4 +1,5 @@
 function wordCount(sentence) {
+	if((sentence === undefined || sentence === null) || (sentence.constructor !== String || sentence.length < 1)) return null;
 	var sentence_copy = sentence + ' ';
 	var word = '';
 	var counter = {};
@@ -8,11 +9,19 @@ function wordCount(sentence) {
 		if (break_chars.indexOf(sentence_copy[i]) === -1) {
 			word += sentence_copy[i];
 		} else {
-			console.log(word.toString());
+			
 			if (word !== ''){
-				counter[word] = (counter[word] !== undefined) ? counter[word] + 1: 1;
-				console.log(counter);
-				word = '';
+																			
+				if ((counter[word] !== undefined && counter[word].constructor != Number)){
+						counter[word] = 1;
+						console.log(counter[word]);
+						word = '';
+					
+				} else {
+					counter[word] = (counter[word] !== undefined) ? counter[String(word)] + 1: 1;
+					//console.log(counter);		
+					word = '';	
+				}
 			}
 		}
 	}
